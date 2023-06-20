bd7bd3c8c51dddd949ff04f9aeb57eb980d0a31e
Guilherme Virgilli <gsvirgilli@gmail.com>
2023-06-20 19:43:28 -0300

    Requisito 2°/incompleto/completo

---
+++ src/components/Album.tsx
@@ -0,0 +1,7 @@
+function Album() {
+  return (
+    <h1>Carregando...</h1>
+  );
+}
+
+export default Album;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,79 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import { AlbumType } from '../types';
+
 function Search() {
+  const [artistName, setArtistName] = useState('');
+  const [nomeArtista, setNomeArtist] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [albums, setAlbums] = useState<AlbumType[]>([]);
+  const [searchCompleted, setSearchCompleted] = useState(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setArtistName(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    if (artistName.length >= 2) {
+      setLoading(true);
+      setNomeArtist(artistName);
+      const response = await searchAlbumsAPI(artistName);
+      setAlbums(response);
+      setLoading(false);
+      setArtistName('');
+      setSearchCompleted(true);
+    }
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    return event?.preventDefault();
+  };
+
   return (
-    <h2>GUi!</h2>
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <h1>Pesquisar Artista</h1>
+        <input
+          type="text"
+          value={artistName}
+          onChange={handleInputChange}
+          data-testid="search-artist-input"
+        />
+        <button
+          onClick={handleSearch}
+          disabled={artistName.length < 2}
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
+      </form>
+      {loading && <p>Carregando...</p>}
+
+      {searchCompleted && (
+        <div>
+          {albums.length > 0 ? (
+            <>
+              <p>Resultado de álbuns de: {nomeArtista}</p>
+              <ul>
+                {albums.map((album) => (
+                  <li key={album.collectionId}>
+                    <Link
+                      to={`/album/${album.collectionId}`}
+                      data-testid={`link-to-album-${album.collectionId}`}
+                    >
+                      {album.collectionName}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <p>Nenhum álbum foi encontrado.</p>
+          )}
+        </div>
+      )}
+    </>
   );
 }
 
